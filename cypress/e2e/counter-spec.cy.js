e584192957a9774724fe165ca4bb01cb869c28df
Viperbi <nickels.florian@gmail.com>
2024-12-02 16:03:38 +0100

    Ajout de counter-spec.cy.js avec diférrents test

---
+++ cypress/e2e/counter-spec.cy.js
@@ -0,0 +1,55 @@
+describe("Tests de l'application Counter", () => {
+	it("devrait afficher un compte initial de 0", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get("#counter").should("have.text", "count is 0");
+	});
+
+	it("devrait augmenter le compte lors du clic sur le bouton", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get("#counter").click();
+		cy.get("#counter").should("have.text", "count is 1");
+	});
+	it("devrai augmenter le compte de 2 lors de deux clics sur le bouton", () => {
+		cy.visit("http://localhost:5173/");
+		for (let i = 0; i < 2; i++) {
+			cy.get("#counter").click();
+		}
+		cy.get("#counter").should("have.text", "count is 2");
+	});
+	let count = 0;
+	let itération = 30;
+	it(`devrai augmenter le compte de ${itération} lors de ${itération} clics sur le bouton`, () => {
+		cy.visit("http://localhost:5173/");
+		for (let i = 0; i < itération; i++) {
+			count += 1;
+			cy.get("#counter").click();
+		}
+		cy.get("#counter").should("have.text", `count is ${count}`);
+	});
+	it("devrait trouver le lien vite.js sur le logo vite", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get(".logo").eq(0).should("be.visible");
+		cy.get(".logo")
+			.eq(0)
+			.parents("a")
+			.should("have.attr", "href", "https://vite.dev");
+	});
+	it("devrait trouver le lien vite.js sur le logo vite", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get(".logo.vanilla").eq(0).should("be.visible");
+		cy.get(".logo.vanilla")
+			.eq(0)
+			.parents("a")
+			.should(
+				"have.attr",
+				"href",
+				"https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+			);
+	});
+	/**it("devrait augmenter le compte lors du shift clic de 10", () => {
+		cy.visit("http://localhost:5173/");
+		cy.get("body").type("{shift}", { release: false });
+		cy.get("#counter").click();
+		cy.get("#counter").should("have.text", "count is 10");
+	});*/
+});
--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,23 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+	let counter = 0;
+	const setCounter = (count) => {
+		counter = count;
+		element.innerHTML = `count is ${counter}`;
+	};
+	/**let shift = false;
+	window.addEventListener("keypress", (e) => {
+		console.log(e);
+		if (e.keycode == 16) {
+			shiftkey = true;
+			console.log("shift");
+		}
+	});*/
+	element.addEventListener("click", () => {
+		//if (shift == false) {
+		setCounter(counter + 1);
+		/** 	} else {
+		setCounter(counter + 10);
+			}*/
+	});
+	setCounter(0);
 }
